b963e94cd66b714d726c94e3f9f050e94cb3dec9
bcho0023 <127581782+bcho0023@users.noreply.github.com>
2023-04-15 22:10:27 +1000

    Completed Calendar Daily View Base with Base Activity

--- app/src/main/java/com/example/Goalden/CalendarView.java
+++ app/src/main/java/com/example/Goalden/CalendarView.java
@@ -40,7 +40,7 @@ public class CalendarView extends AppCompatActivity implements  CalendarAdapter.
 
     private void setMonthView() {
         monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
-        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray();
 
         CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
